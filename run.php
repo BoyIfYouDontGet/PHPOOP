335b8b3d6ac6bf1ba4f00f73315e44792999668e
Miguel Garcia <miguelgarcia2515@gmail.com>
2015-01-09 08:26:00 -0800

    letcture

--- run.php
+++ run.php
@@ -1,63 +1,45 @@
 <?php
 // example one
-if(is_integer()) {
+
+$number = 34;
+
+if(is_integer($number)===true) {
 	echo "this is an integer";
 }
 else {
 	echo "This is not an interger";
-}
+
 var_dump(is_integer('34'));
 var_dump(is_integer('444'));
-//This is not an intergerbool(false) bool(false)
+//This is an interger
 
-
+}
 
 //example 2
+$array = ('3,4,5');
 
-
-if (is_array()) {
+if (is_array($array)===true) {
 
 	echo "this is an array";
 }
 else {
 	echo "This is not an array";
-}
-var_dump(is_array('34'));
-//This is not an arraybool(false) 
 
+var_dump(is_array('3'));
+//This is not an arraybool(false) 
+}
 // example 3
-if (is_object()){
+$object = ('DED');
+
+if (is_object($object)===true){
 
 	echo "this is an object";
 }
 else {
 	echo "This is not an object";
-}
+
 var_dump(is_object('DED'));
 //This is not an objectbool(false)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 ?>

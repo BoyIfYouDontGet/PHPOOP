f3e301c83762921bf750aac6a90d5b888ced19dc
Miguel Garcia <miguelgarcia2515@gmail.com>
2015-01-23 08:41:00 -0800

    finished

--- lecture5.php
+++ lecture5.php
@@ -25,4 +25,48 @@ class Cars extends Car{
 	}
 }
 $car1
+
+class Animal {
+public $firstName;
+public $lastName;
+public $scientificName;
+public $gender;
+public $weight;
+public $bark;
+public $meow;
+function __construct($scientificName, $firstName, $lastName, $gender, $weight, $bark, $meow) {
+$this­>scientificName = $scientificName;
+$this­>firstName = $firstName;
+$this­>lastName = $lastName;
+$this­>gender = $gender;
+$this­>weight = $weight;
+$this­>bark = $bark;
+$this­>meow = $meow;
+}
+function getName() {
+return “This is my “ . $this­>firstName .
+“ and last ” . $this­>lastName .
+“ and this is my scientific­name “ . $this­>scientificName . “ .”;
+}
+}
+class Cat extends Animal {
+function __construct($scientificName, $firstName, $lastName, $gender, $weight, $meow){
+$this­>meow = $meow;
+}
+function greet() {
+return $this­>meow;
+parent:__:construct($scientificName, $firstName, $lastName, $gender, $weight);
+}
+}
+class Dog extends Animal {
+function __construct($scientificName, $firstName, $lastName, $gender, $weight, $bark) {
+$this­>bark = $bark;
+}
+function hello() {
+return $this­>bark;
+}
+}
+$cat = new Cat(“Cat”, “Mellow”, “Yellow”, “male”, 15, true);
+print “Animal 1 is a ” . $cat­>getName();
+parent:__:construct($scientificName, $firstName, $lastName, $gender, $weight);
 ?>
